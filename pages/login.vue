e41545942a613e258c061569fa80f755ea86129f
Jose Vega <jocvegar@gmail.com>
2021-08-11 07:51:36 -0600

    updates to use google signin

--- pages/login.vue
+++ pages/login.vue
@@ -1,66 +1,38 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Login</div>
-          <div class="card-body">
-            <div v-if="error" class="alert alert-danger">{{ error }}</div>
-            <form action="#" @submit.prevent="login">
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right"
-                  >Email</label
-                >
+  <v-container>
+    <v-alert
+      v-if="error"
+      border="right"
+      colored-border
+      type="error"
+      elevation="2"
+    >
+      {{ errorMessage }}
+    </v-alert>
 
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value
-                    required
-                    autofocus
-                    v-model="form.email"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <label
-                  for="password"
-                  class="col-md-4 col-form-label text-md-right"
-                  >Password</label
-                >
-
-                <div class="col-md-6">
-                  <input
-                    id="password"
-                    class="form-control"
-                    name="password"
-                    required
-                    v-model="form.password"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Login</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    <v-row style="min-height: 50vh">
+      <v-btn
+        color="error"
+        class="pa-8 white--text mx-auto my-auto"
+        @click="googleSignIn()"
+      >
+        Ingresar con Google
+        <v-icon right dark>
+          mdi-google
+        </v-icon>
+      </v-btn>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import firestore from "firebase";
+
 export default {
   data() {
     return {
+      errorMessage: "",
+      error: false,
       form: {
         email: "",
         password: ""
@@ -69,13 +41,23 @@ export default {
     };
   },
   methods: {
-    login() {
+    googleSignIn() {
+      let provider = new firestore.auth.GoogleAuthProvider();
+
       this.$fire.auth
-        .signInWithEmailAndPassword(this.form.email, this.form.password)
-        .catch(error => console.log(`error`, error))
-        .then(user => {
-          $nuxt.$router.push("/admin");
-        });
+        .signInWithPopup(provider)
+        .then(result => {
+          const user = result.user;
+          const isNewUser = result.additionalUserInfo.isNewUser;
+          if (isNewUser) {
+            user.delete();
+            this.errorMessage = "Usuario no existe buahah";
+            this.error = true;
+          } else {
+            $nuxt.$router.push("/admin");
+          }
+        })
+        .catch(error => console.log(`error`, error));
     }
   },
   middleware: "auth"
