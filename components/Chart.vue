6678c9a572e49c69cf56ce910844130a2596930a
Jose Vega <jocvegar@gmail.com>
2021-08-12 08:52:30 -0600

    adds charts

--- components/Chart.vue
+++ components/Chart.vue
@@ -3,43 +3,20 @@ import { Radar } from "vue-chartjs";
 
 export default {
   extends: Radar,
-  middleware: "auth",
-  data: () => ({
+  props: {
     chartdata: {
-      labels: ["Muy Mala", "Mala", "Regular", "Buena", "Muy Buena"],
-      datasets: [
-        {
-          label: "My First Dataset",
-          data: [65, 59, 10, 20, 56],
-          fill: true,
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          borderColor: "rgb(255, 99, 132)",
-          pointBackgroundColor: "rgb(255, 99, 132)",
-          pointBorderColor: "#fff",
-          pointHoverBackgroundColor: "#fff",
-          pointHoverBorderColor: "rgb(255, 99, 132)"
-        },
-        {
-          label: "My Second Dataset",
-          data: [28, 48, 40, 60, 56],
-          fill: true,
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
-          borderColor: "rgb(54, 162, 235)",
-          pointBackgroundColor: "rgb(54, 162, 235)",
-          pointBorderColor: "#fff",
-          pointHoverBackgroundColor: "#fff",
-          pointHoverBorderColor: "rgb(54, 162, 235)"
-        }
-      ]
-    },
+      type: Object,
+      default: null
+    }
+  },
+  data: () => ({
     options: {
       responsive: true,
       maintainAspectRatio: false,
-
       plugins: {
         title: {
           display: true,
-          text: "Chart.js Radar Chart"
+          text: "Fitos Encuesta de Calidad"
         }
       }
     }
--- pages/admin.vue
+++ pages/admin.vue
@@ -1,25 +1,197 @@
 <template>
-  <v-container>
+  <v-container v-if="!loading">
     <client-only placeholder="Cargando...">
-      <div class="chart-container">
-        <chart />
+      <div class="py-5">
+        <h3 class="text-center">Encuesta de Calidad</h3>
+        <br />
+        <div v-if="chartdata" class="chart-container">
+          <chart :chartdata="chartdata" />
+        </div>
       </div>
     </client-only>
+
+    <div>
+      <h3 class="text-center">Sugerencias</h3>
+      <v-data-table
+        :headers="headers"
+        :items="sugerencias"
+        :items-per-page="15"
+        class="elevation-1"
+      >
+        <template v-slot:[`item.createdAt`]="{ item }">
+          {{ item.createdAt | filterDate }}
+        </template>
+      </v-data-table>
+    </div>
   </v-container>
 </template>
 
 <script>
+import moment from "moment-timezone";
+import "moment/locale/es";
+
 export default {
-  middleware: "auth"
+  middleware: "auth",
+  data() {
+    return {
+      loading: true,
+      surveys: [],
+      chartdata: null,
+      headers: [
+        {
+          text: "Fecha",
+          align: "start",
+          value: "createdAt"
+        },
+        { text: "Sugerencia", value: "sugerencia" }
+      ]
+    };
+  },
+  async mounted() {
+    await this.getSurveys();
+    this.getChartData();
+  },
+  methods: {
+    async getSurveys() {
+      const surveyRef = await this.$fire.firestore.collection("surveys");
+      try {
+        const docs = await surveyRef.get();
+        docs.forEach(doc => {
+          this.surveys.push(Object.assign({ id: doc.id }, doc.data()));
+        });
+        this.loading = false;
+      } catch (error) {
+        this.category = [];
+        console.log(`error`, error);
+        this.loading = false;
+      }
+    },
+    getChartData() {
+      this.chartdata = {
+        labels: ["Muy Buena", "Buena", "Regular", "Mala", "Muy Mala"],
+        datasets: [
+          {
+            label: "Calidad",
+            data: this.calidad,
+            fill: true,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgb(255, 99, 132)",
+            pointBackgroundColor: "rgb(255, 99, 132)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgb(255, 99, 132)"
+          },
+          {
+            label: "Presentación",
+            data: this.presentacion,
+            fill: true,
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgb(54, 162, 235)",
+            pointBackgroundColor: "rgb(54, 162, 235)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgb(54, 162, 235)"
+          },
+          {
+            label: "Rapidez",
+            data: this.rapidez,
+            fill: true,
+            backgroundColor: "rgba(255, 205, 86, 0.2)",
+            borderColor: "rgb(255, 205, 86)",
+            pointBackgroundColor: "rgb(255, 205, 86)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgb(255, 205, 86)"
+          },
+          {
+            label: "Limpieza",
+            data: this.limpieza,
+            fill: true,
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgb(75, 192, 192)",
+            pointBackgroundColor: "rgb(75, 192, 192)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgb(75, 192, 192)"
+          },
+          {
+            label: "Mesero",
+            data: this.mesero,
+            fill: true,
+            backgroundColor: "rgba(153, 102, 255, 0.2)",
+            borderColor: "rgb(153, 102, 255)",
+            pointBackgroundColor: "rgb(153, 102, 255)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgb(153, 102, 255)"
+          }
+        ]
+      };
+    },
+    surveyItemCount(object, calification) {
+      const item = object[calification];
+      return item?.length ? item?.length : 0;
+    },
+    getCount(attr) {
+      return {
+        muy_buena: this.surveyItemCount(
+          this._.groupBy(this.surveys, attr),
+          "muy_buena"
+        ),
+        buena: this.surveyItemCount(
+          this._.groupBy(this.surveys, attr),
+          "buena"
+        ),
+        regular: this.surveyItemCount(
+          this._.groupBy(this.surveys, attr),
+          "regular"
+        ),
+        mala: this.surveyItemCount(this._.groupBy(this.surveys, attr), "mala"),
+        muy_mala: this.surveyItemCount(
+          this._.groupBy(this.surveys, attr),
+          "muy_mala"
+        )
+      };
+    }
+  },
+  computed: {
+    calidad() {
+      return Object.values(this.getCount("calidad"));
+    },
+    presentacion() {
+      return Object.values(this.getCount("presentacion"));
+    },
+    rapidez() {
+      return Object.values(this.getCount("rapidez"));
+    },
+    limpieza() {
+      return Object.values(this.getCount("limpieza"));
+    },
+    mesero() {
+      return Object.values(this.getCount("mesero"));
+    },
+    sugerencias() {
+      return this.surveys.filter(survey => survey.sugerencia.length > 0);
+    }
+  },
+  filters: {
+    filterDate(timestamp) {
+      if (!timestamp) return "no disponible";
+      return moment(timestamp.toDate())
+        .tz("America/Tegucigalpa")
+        .format("L");
+    }
+  }
 };
 </script>
+
 <style lang="scss">
 .chart-container {
   flex-grow: 1;
   min-height: 0;
   > div {
     position: relative;
-    height: 100%;
+    height: 60vh;
   }
 }
 </style>
